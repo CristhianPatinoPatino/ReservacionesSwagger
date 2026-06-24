5fcf7e11c986fc84800d2053f7071149fd111e1a
Cristhian <cristhanatz@gmail.com>
2026-06-24 16:35:36 -0600

    agregando ip para el front en SecurityConfig

--- src/main/java/org/example/ejercicio08reservaciones/core/config/SecurityConfig.java
+++ src/main/java/org/example/ejercicio08reservaciones/core/config/SecurityConfig.java
@@ -59,7 +59,9 @@ public class SecurityConfig {
                 "http://localhost:5173", 
                 "http://localhost:3000",
                 "http://127.0.0.1:5173",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "http://localhost:4200",
+                "http://127.0.0.1:4200/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
